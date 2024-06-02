4b663da0d1521123824b0b0a73cab7c056a9fa30
Eduard Sänger <esaenger@sievers-group.com>
2024-06-03 00:45:54 +0200

    Version 1.0 Commit

--- README.md
+++ README.md
@@ -1,7 +1,50 @@
-Documentation
-============
+# To-Do-Listen Verwaltung in einer WebApp (Bereitstellung über Raspberry Pi)
 
-Wall of Text
+## Überblick
 
+Die ToDo-Listen-Verwaltung Web-App ermöglicht es Benutzern, To-Do-Listen und -Einträge zu erstellen, zu aktualisieren, zu löschen und abzurufen. Diese Web-App stellt eine RESTful API zur Verfügung, die mithilfe von Flask implementiert wurde.
 
--------
+## Hauptfunktionen
+
+- **List Management**
+  - Erstellen neuer To-Do-Listen
+  - Abrufen aller To-Do-Einträge einer bestimmten Liste
+  - Löschen einer To-Do-Liste
+
+- **Entry Management**
+  - Hinzufügen neuer Einträge zu einer Liste
+  - Aktualisieren vorhandener Einträge
+  - Löschen vorhandener Einträge
+
+## Ausrollen der Web-App mit Docker
+
+Um die Web-App auf einem Raspberry Pi bereitzustellen, wird Docker verwendet. Folgen Sie diesen Schritten um die Web-App auszurollen:
+
+### Vorbereitungen
+
+1. **Installieren von Docker auf dem Raspberry Pi**
+
+   Zunächst müssen Sie Docker auf Ihrem Raspberry Pi installieren. Verbinden Sie ihren Pi dazu mit einem Internetfähigen Netzwerk. Führen Sie dazu über Bash die folgenden Befehle aus:
+
+   curl -sSL <https://get.docker.com> | sh
+   sudo usermod -aG docker pi
+
+   (Bitte entfernen Sie die <> an der URL bevor Sie den Befehl verwenden)
+
+2. **Installieren von Docker Compose**
+
+    Installieren Sie Docker Compose, um den bereitgestellten Container zu orchestrieren. Führen Sie dazu über Bash die folgenden Befehle aus:
+
+    sudo apt-get install -y python3-pip
+    sudo pip3 install docker-compose
+
+3. **Bereitstellung der Web-App**
+
+    - Kopieren Sie alle Dateien des Repositories, einschließlich der docker-compose.yml, auf die Micro-SD-Karte.
+    - Setzen Sie die Micro-SD-Karte in den Raspberry Pi ein und starten Sie ihn. Wechseln Sie über den Befehl cd in das Verzeichnis, das die docker-compose.yml Datei enthält, und führen Sie, um die Web-App im Hintergrund zu starten, den folgenden Befehl aus:
+
+    sudo docker-compose up -d
+
+### Zugriff auf die Web-App
+
+    Die Web-App ist jetzt bereit und kann über den Swagger-Editor aufgerufen und getestet werden. Öffnen Sie den Swagger-Editor unter 'editor.swagger.io' in Ihrem Browser und kopieren Sie den Inhalt der Specification_ToDo.yaml in den Editor. Die API-Spezifikation wird geladen und Sie können die Endpunkte der Listenverwaltung testen.
--- project_webserver/ToDoList.py
+++ project_webserver/ToDoList.py
@@ -1,16 +1,7 @@
-
-#*
-#Example script showing how to represent todo lists and todo entries in Python
-#data structures and how to implement endpoint for a REST API with Flask.
-
-#Requirements:
-#* flask
-#
-
+#Python script to handle different type of operations on todo lists
 import uuid 
 
 from flask import Flask, request, jsonify, abort
-
 
 # initialize Flask server
 app = Flask(__name__)
@@ -38,15 +29,16 @@ todos = [
 ]
 
 # add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
+# add Patch as possible http method
 @app.after_request
 def apply_cors_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE,PATCH'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,DELETE'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
 # define endpoint for getting and deleting existing todo lists
-@app.route('/list/<list_id>', methods=['GET', 'DELETE'])
+@app.route('/todo-list/<list_id>', methods=['GET', 'DELETE'])
 def handle_list(list_id):
     # find todo list depending on given list id
     list_item = None
@@ -67,9 +59,8 @@ def handle_list(list_id):
         todo_lists.remove(list_item)
         return '', 200
 
-
 # define endpoint for adding a new list
-@app.route('/list', methods=['POST'])
+@app.route('/todo-list', methods=['POST'])
 def add_new_list():
     # make JSON from POST data (even if content type is not set correctly)
     new_list = request.get_json(force=True)
@@ -77,14 +68,56 @@ def add_new_list():
     # create id for new list, save it and return the list with id
     new_list['id'] = uuid.uuid4()
     todo_lists.append(new_list)
-    return jsonify(new_list), 200
+    return jsonify(new_list), 201
 
+# define endpoint for adding additional entries
+@app.route('/todo-list/<list_id>/entry', methods=['POST'])
+def add_new_entry(list_id):
+    #check if given list id exists
+    if not any(l['id'] == list_id for l in todo_lists):
+        abort(406)
+    #json for post entry
+    new_entry = request.get_json(force=True)
+    print('New entry has been added: {}'.format(new_entry))
+    # create new entry
+    new_entry['id'] = uuid.uuid4()
+    new_entry['list'] = list_id
+    todos.append(new_entry)
+    return jsonify(new_entry), 201
 
-# define endpoint for getting all lists
-@app.route('/lists', methods=['GET'])
-def get_all_lists():
-    return jsonify(todo_lists)
+# find entry function to utilize in update and delete function
+# create and entry item if there is one with corresponding IDs
+def find_entry(list_id, entry_id):
+    for entry in todos:
+        if entry['id'] == entry_id and entry['list_id'] == list_id:
+            return entry
+    return None
 
+# define endpoint for updating existing entries 
+@app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['PATCH'])
+def update_entry(list_id, entry_id):
+    # use finding method and check if something was found
+    entry_item = find_entry(list_id, entry_id)
+    if not entry_item:
+        abort(406)
+    # create update data, remove old data and save updated data
+    update_data = request.get_json(force=True)
+    update_data['id'] = entry_item['id']
+    update_data['list_id'] = entry_item['list_id']
+    todos.remove(entry_item)
+    todos.append(update_data)
+    return jsonify(update_data), 200
+
+#define endpoint for deleting existing entries
+@app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['DELETE'])
+def delete_entry(list_id, entry_id):
+    # use finding method and check if something was found
+    entry_item = find_entry(list_id, entry_id)
+    if not entry_item:
+        abort(404)
+    # remove undesired entry
+    todos.remove(entry_item)
+    return '', 200
 
 if __name__ == '__main__':
     # start Flask server
